4c4ef0997e0872fe26dccb34255fb1ffcd773fbe
lindsay-aic <lindsay.aichinger@gmail.com>
2020-04-02 05:06:25 -0700

    Adding React Ocean Mineral Build.

---
+++ app/precache-manifest.b11b33c1ca15ee3c37cb6ecd4a99cb1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a770ac174579592799398928cd443b1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ace7781874393b0018b",
+    "url": "/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "e2817117d1718b4d562b",
+    "url": "/static/css/main.76dd18f7.chunk.css"
+  },
+  {
+    "revision": "1ace7781874393b0018b",
+    "url": "/static/js/2.2edb6244.chunk.js"
+  },
+  {
+    "revision": "e2817117d1718b4d562b",
+    "url": "/static/js/main.d4b924c5.chunk.js"
+  },
+  {
+    "revision": "7eaf56ca30e86350a200",
+    "url": "/static/js/runtime-main.c3a5dfa4.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);
